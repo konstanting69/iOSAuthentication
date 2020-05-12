a197f3771ab6d34fcd079699acd65eedfdd85a20
Konstantin <konstanting69@gmail.com>
2020-05-12 09:39:42 +0100

    Initial Commit

---
+++ PPWAuthenticationKonstantinGeorgiev/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PPWAuthenticationKonstantinGeorgiev
+//
+//  Created by Konstantin Georgiev on 12/05/2020.
+//  Copyright © 2020 Konstantin Georgiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
